6dd5256130ee75a4816467d57f978e5ed2c06675
bigolboyyo <wesley.grant94@gmail.com>
2022-07-06 13:46:40 -0400

    pry_it_up

---
+++ pry_demo.rb
@@ -0,0 +1,31 @@
+require 'pry' # REMEMBER BACKTICKS ARE NOT THE SAME IN RUBY AS THEY ARE IN JAVASCRIPT
+
+# PRY is an alternative to IRB : https://github.com/pry/pry
+
+# PRY creates a breakpoint and lets you "pry open your code"
+
+fruits = ["bananas", "apples", "oranges", ["strawberries", "raspberries", ["tomato"]]]
+
+def return_the_tomato(array)
+    # Example of a pry that requires scrolling
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+    binding.pry
+end
+
+return_the_tomato(fruits)
+
+
