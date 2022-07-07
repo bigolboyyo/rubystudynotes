0703c8f1d955dc81e2640b71daba3b48d644d6d2
bigolboyyo <wesley.grant94@gmail.com>
2022-07-07 13:16:32 -0400

    operators

---
+++ control_flow_operators.rb
@@ -0,0 +1,19 @@
+# Use common comparison methods for control flow (==, !=, >, <)
+# Use common operators for control flow (&&, ||, !, ?:)
+
+#FIXME: Conditional operators: Generally in any language this means that you will be running certain code when/if some condition has been met. 
+
+# Comparison Operators
+
+# - ( > ) - Greater than
+# - ( >= ) - Greater than OR equal to
+# - ( < ) - Less than
+# - ( <= ) - Less than OR equal to
+# - ( == ) - Equal to
+# - ( != ) - NOT equal to
+
+# - ( && ) - Logical and. Are both values truthy?
+# - ( || ) - Logical or. Is one or the other value truthy?
+# - ( ! ) - not. Coerces the data to its boolean equivalent, then reverses it (true becomes false, and vice versa).
+
+# Ruby also has the ternary operator (?:) for writing an inline conditional statement:
